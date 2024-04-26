90d9f8752c156fc6b58a266573c6d801a2c8c5a3
lgandarillas <lgandaf@gmail.com>
2024-04-26 11:58:17 +0200

    add: src/main.c

---
+++ src/main.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgandari <lgandari@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/26 11:50:28 by lgandari          #+#    #+#             */
+/*   Updated: 2024/04/26 11:57:34 by lgandari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/push_swap.h"
+
+// Create parser
+#include <stdio.h> // delete
+
+int	main(int argc, char **argv)
+{
+	t_stack_node	*a;
+	t_stack_node	*b;
+
+	a = NULL;
+	b = NULL;
+	if (argc == 1)
+		return (1);
+	else if (argc == 2 && !argv[1][0])
+		return (1);
+	else if (argc == 2)
+		printf("Hola\n"); // delete
+		//argv = improved_split(argv[1], ' ');
+	//init_stack_a(&a, argv + 1);
+	//if (!stack_sorted(a))
+	//{
+	//	if (stack_len(a) == 2)
+	//		sa(&a, false);
+	//	else if (stack_len(a) == 3)
+	//		sort_three(&a);
+	//	else
+	//		sort_stacks(&a, &b);
+	//}
+	//free_stack(&a);
+	return (0);
+}
