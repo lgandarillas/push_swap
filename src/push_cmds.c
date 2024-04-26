263882a4cdd95c02ae663a2904e47b9676dfb054
Luis Gandarillas Fernandez <lgandaf@gmail.com>
2024-04-26 20:36:52 +0200

    add: src/push_cmds.c (not finished)

---
+++ src/push_cmds.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_cmds.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgandari <lgandari@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/26 20:14:33 by lgandari          #+#    #+#             */
+/*   Updated: 2024/04/26 20:36:01 by lgandari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/push_swap.h"
+
+void	push(t_stack_node **a, t_stack_node **b)
+{
+	
+}
+
+void	pa(t_stack_node **a, t_stack_node **b, bool print)
+{
+	push(a, b);
+	if (print)
+		ft_printf("pa\n");
+}
+
+void	pb(t_stack_node **a, t_stack_node **b, bool print)
+{
+	push(b, a);
+	if (print)
+		ft_printf("pb\n");
+}
