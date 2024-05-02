9b042f08d4067ef818575d387095a969f8fe85df
lgandarillas <lgandaf@gmail.com>
2024-05-02 20:01:30 +0200

    rotate_cmds

--- src/sort_stacks.c
+++ src/sort_stacks.c
@@ -6,11 +6,43 @@
 /*   By: lgandari <lgandari@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 19:19:27 by lgandari          #+#    #+#             */
-/*   Updated: 2024/05/02 19:22:36 by lgandari         ###   ########.fr       */
+/*   Updated: 2024/05/02 19:54:51 by lgandari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
+
+static void	move_a_to_b(t_stack_node **a, t_stack_node **b)
+{
+	t_stack_node	*cheapest_node;
+
+	cheapest_node = get_cheapest(*a);
+	if (cheapest_node->above_median && cheapest_node->target->node->above_median)
+		rotate_both(a, b, cheapest_node);
+	else if (!(cheapest_node->above_median)
+			&& !(cheapest_node->target_node->above_median))
+		rev_rotate_both(a, b, cheapest_node);
+	prep_for_push(a, cheapest_node, 'a');
+	prep_for_push(b, cheapest_node->target_node, 'b');
+	pb(b, a, false);
+}
+
+static void	move_b_to_a(t_stack_node **a, t_stack_node **b)
+{
+	prep_for_push(a, (*b)->target_node, 'a');
+	pa(a, b, false);
+}
+
+static void	min_on_top(t_stack_node **a)
+{
+	while ((*a)->num != find_min(*a)->num)
+	{
+		if (find_min(*a)->above_median)
+			ra(a, false);
+		else
+			rra(a, false);
+	}
+}
 
 void	sort_stacks(t_stack_node **a, t_stack_node **b)
 {
