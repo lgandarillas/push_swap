3e696cf9508cf173ed08e4fc3977747157c6ceeb
lgandarillas <lgandaf@gmail.com>
2024-06-01 11:04:58 +0200

    Delete old files.

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: lgandari <lgandari@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 11:50:28 by lgandari          #+#    #+#             */
-/*   Updated: 2024/05/04 11:37:39 by lgandari         ###   ########.fr       */
+/*   Updated: 2024/06/01 11:03:00 by lgandari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,32 +21,3 @@ int	main(int argc, char **argv)
 	free_split(cmds);
 	return (0);
 }
-
-/*
-int	main(int argc, char **argv)
-{
-	t_stack_node	*a;
-	t_stack_node	*b;
-
-	a = NULL;
-	b = NULL;
-
-	//if (argc == 1 || (argc == 2 && !argv[1][0]))
-	//	return (1);
-	//else if (argc == 2)
-	//	argv = ft_split(argv[1], ' ');
-	//init_stack_a(&a, argv + 1);
-
-	if (!stack_sorted(a))
-	{
-		if (stack_len(a) == 2)
-			sa(&a, false);
-		else if (stack_len(a) == 3)
-			sort_three(&a);
-		else
-			sort_stacks(&a, &b);
-	}
-	free_stack(&a);
-	return (0);
-}
-*/
