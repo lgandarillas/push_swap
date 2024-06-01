0dea24c9dbe923aba57772d980d9be3370349234
lgandarillas <lgandaf@gmail.com>
2024-06-01 16:58:53 +0200

    Inital sort algorithms.

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: lgandari <lgandari@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 11:50:28 by lgandari          #+#    #+#             */
-/*   Updated: 2024/06/01 16:46:49 by lgandari         ###   ########.fr       */
+/*   Updated: 2024/06/01 16:52:57 by lgandari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,14 +26,14 @@ int	main(int argc, char **argv)
 	print_stack(a);
 	ft_printf("Is sorted = %d\n", is_sorted(a));
 	/*
-	if (!stack_sorted(a))
+	if (!is_sorted(a))
 	{
 		if (stack_len(a) == 2) -> OK
 			sa(&a, false);
 		else if (stack_len(a) == 3)
 			sort_three(&a);
 		else
-			sort_stacks(&a, &b);
+			turk_sort(&a, &b);
 	}
 	*/
 	free_stack(a);
--- src/sort.c
+++ src/sort.c
@@ -6,11 +6,54 @@
 /*   By: lgandari <lgandari@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/01 16:39:13 by lgandari          #+#    #+#             */
-/*   Updated: 2024/06/01 16:44:01 by lgandari         ###   ########.fr       */
+/*   Updated: 2024/06/01 16:58:08 by lgandari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
+
+// REVISAR - sort.c
+/*
+void	sort_three(t_stack_node **a)
+{
+	t_stack_node	*biggest_node;
+
+	biggest_node = find_max(*a);
+	if (biggest_node == *a)
+		ra(a);
+	else if ((*a)->next == biggest_node)
+		rra(a);
+	if ((*a)->num > (*a)->next->num)
+		sa(a);
+}
+*/
+
+// REVISAR - sort.c
+/*
+void	turk_sort(t_stack_node **a, t_stack_node **b)
+{
+	int	len_a;
+
+	len_a = stack_len(*a);
+	if (len_a-- > 3 && !is_sorted(*a))
+		pb(b, a);
+	if (len_a-- > 3 && !is_sorted(*a))
+		pb(b, a);
+	while (len_a-- > 3 && !is_sorted(*a))
+	{
+		init_nodes_a(*a, *b);
+		move_a_to_b(a, b);
+	}
+	sort_three(a);
+	while (*b)
+	{
+		init_nodes_b(*a, *b);
+		move_b_to_a(a, b);
+	}
+	current_index(*a);
+	min_on_top(a);
+}
+*/
 
 int	is_sorted(t_stack_node *s)
 {
