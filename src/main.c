dc8b97f5459908d54f1dedf9b20a09217c137382
lgandarillas <lgandaf@gmail.com>
2024-06-03 19:00:56 +0200

    Fix style errors in main.c

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: lgandari <lgandari@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 11:50:28 by lgandari          #+#    #+#             */
-/*   Updated: 2024/06/03 18:55:32 by lgandari         ###   ########.fr       */
+/*   Updated: 2024/06/03 18:59:02 by lgandari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,7 @@ int	main(int argc, char **argv)
 	b = NULL;
 	args = parser(argc, argv);
 	init_stack(&a, args);
-	print_stack(a);	//Delete
-	ft_printf("Is sorted = %d\n", is_sorted(a));	// Delete
+	print_stack(a);
 	if (has_duplicates(a))
 		stack_error(a, args);
 	if (!is_sorted(a))
@@ -35,8 +34,7 @@ int	main(int argc, char **argv)
 		else
 			turk_sort(&a, &b);
 	}
-	ft_printf("SORTED.\n"); // Delete
-	print_stack(a);	// Delete
+	print_stack(a);
 	free_stack(a);
 	free_split(args);
 	return (0);
